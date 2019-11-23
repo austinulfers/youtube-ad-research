2ef332034ccd18ecf96b9166ac07b002a3fe58f6
Austin Ulfers <austinulfers@gmail.com>
2019-11-23 11:43:18 -0800

    added minimum features

---
+++ src/read_input.py
@@ -0,0 +1,6 @@
+
+def get_input(filepath, n):
+    #Static Project Input Variables
+    input = open(filepath, 'r')
+    input_seperated = input.read().split(';')
+    return input_seperated[n].split('=')[-1].strip()
